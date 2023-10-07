71b803a4fa047cc8c226c4c2076a60c079c74c85
ratulhasan001 <ratulhasan2108@gmail.com>
2023-10-07 23:19:36 +0600

    DSA

--- parenthesis_balance.cpp
+++ parenthesis_balance.cpp
@@ -1,38 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int evaluate(string str) {
-    stack<int>s;
-    for(int i = 0; i < (int)str.size(); i++) {
-        if(isdigit(str[i])) s.push(str[i] - '0');
-        else {
-            int a = s.top();
-            s.pop();
-            int b = s.top();
-            s.pop();
-            switch(str[i]) {
-                case '+':
-                    s.push(b + a);
-                    break;
-                case '-':
-                    s.push(b - a);
-                    break;
-                case '/':
-                    s.push(b / a);
-                    break;
-                case '*':
-                    s.push(b * a);
-                    break;
+int32_t main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr); 
+    
+    int t; cin >> t;
+    for(int testCase=1; testCase<=t; testCase++) {
+    
+        string s; cin >> s;
+        if((int)s.size() & 1 or s[0] == ')' or s[0] == '}' or s[0] == ']') {
+            cout << "NO" << '\n';
+            continue;
+        } 
+        stack<char>c;
+        for(int i = 0; i < (int)s.size(); i++) {
+            if(c.empty()) c.push(s[i]); 
+            
+            else if(c.top() == '(' and s[i] == ')') {
+                c.pop();
             }
+            else if(c.top() == '{' and s[i] == '}') {
+                c.pop();
+            }
+            else if(c.top() == '[' and s[i] == ']') {
+                c.pop();
+            }
+            else c.push(s[i]);
+
         }
+        (c.empty())? cout << "YES\n" : cout <<"NO\n";
+    
     }
-    return s.top();
+    return 0;
 }
 
-int main() {
-    string s;
-    cin >> s;
-    cout << evaluate(s) << '\n';
-    return 0;
-    // 231*+9-     -4
-}
--- sort_merge.cpp
+++ sort_merge.cpp
@@ -1,9 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 void merge(int a[], int l, int m, int r) {
-
+    int leftSize = m - l + 1;
+    int rightSize = r - m;
+    int L[leftSize], R[rightSize];
+    int k = 0;
+    for(int i = l; i <= m; i++) {
+        L[k] = a[i];
+        k++;
+    }
+    k = 0;
+    for(int i = m + 1; i <= r; i++) {
+        R[k] = a[i];
+        k++;
+    }
+    int i = 0, j = 0;
+    int cur = l;
+    while(i < leftSize and j < rightSize) {
+        if(L[i] <= R[j]) {
+            a[cur] = L[i];
+            i++;
+        }
+        else  {
+            a[cur] = R[j];
+            j++;
+        }
+        cur++;
+    }
+    while(i < leftSize) {
+        a[cur] = L[i];
+        i++;
+        cur++;
+    }
+    while(j < rightSize) {
+        a[cur] = R[j];
+        j++;
+        cur++;
+    }
 }
 
 void merge_sort(int a[], int l, int r) {
@@ -24,7 +58,7 @@ int32_t main() {
     cin >> n;
     int a[n];
     for(int i = 0; i < n; i++) cin >> a[i];
-    // merge_sort(a, n);
+    merge_sort(a, 0, n - 1);
     for(int i = 0; i < n; i++) cout << a[i] << " ";
 
     return 0;
